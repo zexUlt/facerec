29a611366c5f0bd84937ceb1747ce6232c83f707
zexUlt <danil.chernozubov66@gmail.com>
2019-11-14 01:09:02 +0500

    Added separator between video- and photolayers

--- UI/start.py
+++ UI/start.py
@@ -1,8 +1,18 @@
 from tkinter import *
 from mainlayer import MainLayer
 from photolayer import PhotoLayer
+from videolayer import VideoLayer
+from tkinter.ttk import Separator
 
-root = Tk()
-photo = PhotoLayer(root)
-root.mainloop()
-print(MainLayer.photos)
+def main():
+    root = Tk()
+    photo = PhotoLayer(root)
+
+    Separator(root, orient=VERTICAL).grid(padx=10,sticky=N+S,column=1,row=0)
+
+    video = VideoLayer(root)
+    root.mainloop()
+    print(MainLayer.photos)
+
+if __name__ == "__main__":
+    main()
