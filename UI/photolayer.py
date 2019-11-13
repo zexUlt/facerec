97389b5a37e351fb4ea43c5a1b535ddd4f832b93
zexUlt <danil.chernozubov66@gmail.com>
2019-11-13 19:00:17 +0500

    Added selection from listbox

--- UI/photolayer.py
+++ UI/photolayer.py
@@ -9,7 +9,8 @@ class PhotoLayer(MainLayer):
         self.frame = Frame(tk_o)
         self.lb_frame = Frame(self.frame)
         self.lb = Listbox(self.lb_frame)
-
+        self.lb.bind('<<ListboxSelect>>', self.currSelect)
+        
         self.input_form = Entry(self.frame, textvariable=self.tk_path)
         self.addtolist_but = Button(self.frame, text="submit", command=self.update_list)
 
@@ -26,6 +27,10 @@ class PhotoLayer(MainLayer):
     def update_list(self):
         self.save_path(self.tk_path.get())
         self.lb.insert(END, self.tk_path.get())
+        self.input_form.delete(0,END)
     
+    def currSelect(self, evt):
+        print(self.lb.get(self.lb.curselection()))
+
     # --- Send photo to database 
     # def sender(self): 
