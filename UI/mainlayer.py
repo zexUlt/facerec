89d3587cde3a363023cd835c7005e3113866c557
zexUlt <47913457+zexUlt@users.noreply.github.com>
2019-11-26 12:43:48 +0500

    Delete mainlayer.py

--- UI/mainlayer.py
+++
@@ -1,9 +0,0 @@
-class Style:
-    BUT_ALIAS_1 = {"relief": "raised","borderwidth":"3", "bg":"black", "fg":"white", "pady" : 5} # Submit button.
-    BUT_ALIAS_2 = {"relief": "raised","borderwidth":"3", "bg":"black", "fg":"white", "pady" : 0}
-
-class MainLayer:
-    photos = list()
-    videos = dict()    
-
-    
